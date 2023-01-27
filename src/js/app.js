0ffbdf1b379edaab7a51752fb49feb3aa6a33a41
Klim <klim_somov@mail.ru>
2023-01-27 23:43:14 +0300

    klmn

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,21 @@
 следует его расскоментировать
 */
 
+const iconMenu = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const headerMenu = document.querySelector('.header__menu');
+    iconMenu.addEventListener("click", e => {
+        headerMenu.classList.toggle('_active');
+    });
+}
+
+const iconMenub = document.querySelector('.menu-icon');
+if (iconMenu) {
+    const icon = document.querySelector('.menu-icon');
+    iconMenub.addEventListener("click", e => {
+        icon.classList.toggle('_active-b');
+    });
+};
 // Включить/выключить FLS (Full Logging System) (в работе)
 window['FLS'] = true;
 
